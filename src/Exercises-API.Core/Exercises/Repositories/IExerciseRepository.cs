f6c1022ffe7271c3e9888456c1cf6584266e413c
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-05-10 16:59:18 +0400

    Add exercise entity and base repository

---
+++ src/Exercises-API.Core/Exercises/Repositories/IExerciseRepository.cs
@@ -0,0 +1,13 @@
+namespace Exercises_API.Core.Repositories;
+
+using Exercises_API.Core.Exercises.Models;
+
+public interface IExerciseRepository
+{
+    Task<IEnumerable<Exercise>?> GetAllAsync();
+    Task CreateAsync(Exercise exercise);
+    Task DeleteAsync(int id);
+    Task UpdateAsync(int id, Exercise exercise);
+    Task<Exercise> GetByIdAsync(int id);
+    Task ApproveAsync(int id);
+}
